bdc3982c253246df55379f274252e5a023d54b39
Christopher Widling <christopher.widling@gmail.com>
2024-11-12 18:27:08 +0100

    Scrap old main and implement advanced command parsing

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
-from queue import SimpleQueue
 import tkinter as tk
-import multiprocessing, time
+import multiprocessing, time, sys, signal
 from pyghthouse.ph import Pyghthouse
-import pyghthouse.utils as utils
+from local_display import DisplayProcess
+from stopwatch import Stopwatch
+from color_functions import interpolate, cycle
+from os import getenv
 from mp_firework import Fireworks
 from mp_bouncers import BounceAnimation as Bouncers
 from mp_lavablob import Lavablobs
@@ -10,199 +12,167 @@ from mp_rgbtest import RgbTest
 from mp_rain import RainAnimation
 from mp_rebound import ReboundAnimation
 from mp_diffraction import DiffAnimation
-from stopwatch import Stopwatch
+from mp_conway import GameOfLife
+from mp_scrolltext import ScrollText
 
+class AnimationController():   
+    def __init__(self, time_per_anim, gui = False, remote = True, user=None, token=None, fps=60, animation=None) -> None:
+        self.keep_going = True
+        self.displayqueue = None
+        self.name = "JhController"
+        signal.signal(signal.SIGINT, self._handle_sigint)
+        self.gui_enabled = gui
+        self.remote_enabled = remote
+        self.ph = None
+        self.displayprocess = None
+        self.user = user
+        self.token = token
+        self.run(time_per_anim)
 
-timer = Stopwatch()
-
-def read_auth(filename="auth.txt"):
-    with open(filename) as src:
-        username, token = None, None
-        lines = src.readlines()
-        for line in lines:
-            line = line.split(":")
-            if len(line) == 2:
-                match line[0].strip().lower():
-                    case 'name':
-                        username = line[1].strip()
-                        print(f"Username read from file: {username}")
-                    case 'username':
-                        username = line[1].strip()
-                        print(f"Username read from file: {username}")
-                    case 'token':
-                        token = line[1].strip()
-                        print(f"Token read from file: {token}")
-                    case _:
-                        print(f"Unrecognized Key: '{line[0]}'")
-        if not username or not token:
-            print(f"Error: File {filename} is incomplete!")
-        return username, token
-
-class ScalableCanvas(tk.Canvas):
-    def __init__(self, master=None, **kwargs):
-        tk.Canvas.__init__(self, master, **kwargs)
-        self.original_width = 28
-        self.original_height = 28
-        self.scale_factor = 10
-        self.y_distortion = 1.2
-        self.scale_canvas(self.scale_factor)
-        self.localqueue = SimpleQueue()
-        self.fps_target = 10
-        self.started = time.monotonic()
-        self.timer = Stopwatch()
-        self.timer.set(10)
-
-    def scale_canvas(self, factor):
-        self.scale_factor = factor
-        self.config(width=self.original_width * factor, height=self.original_height * factor * self.y_distortion)
-        self.master.geometry(f"{int((self.original_width + 2) * factor)}x{int((self.original_height + 3) * factor * self.y_distortion)}")
-
-    def reset_counter(self):
-        anim_fps = self.frames_queued / (time.monotonic() - self.started)
-        view_fps = self.frames_displayed / (time.monotonic() - self.started)
-        self.frames_queued = anim_fps
-        self.frames_displayed = view_fps
-        self.started = time.monotonic()-1
+    def read_auth(self, filename="auth.txt"):
+        with open(filename) as src:
+            username, token = None, None
+            lines = src.readlines()
+            for line in lines:
+                line = line.split(":")
+                if len(line) == 2:
+                    match line[0].strip().lower():
+                        case 'name':
+                            username = line[1].strip()
+                            print(f"Username read from file: {username}")
+                        case 'username':
+                            username = line[1].strip()
+                            print(f"Username read from file: {username}")
+                        case 'token':
+                            token = line[1].strip()
+                            print(f"Token read from file: {token}")
+                        case _:
+                            print(f"Unrecognized Key: '{line[0]}'")
+            if not username or not token:
+                print(f"Error: File {filename} is incomplete!")
+            else:
+                self.username = username
+                self.token = token
+                return True
+        return False
         
+    def send_frame(self, image: list, factor: float):
+        img = Pyghthouse.empty_image()
+        for x in range(min(len(img), len(image[0]))):
+            for y in range(min(len(img[0]), len(image))):
+                img[x][y] = interpolate(image[y][x], img[x][y], factor) if factor < 1.0 else image[y][x]
+        if self.ph:
+            self.ph.set_image(img)
+        if self.displayqueue:
+            self.displayqueue.put_nowait(img)
         
-def stretch_matrix(matrix):
-    stretched_matrix = []
-
-    for row in matrix:
-        stretched_row = []
-        for pixel in row:
-            stretched_row.append(pixel)
-            stretched_row.append(pixel)  # Füge den Pixel noch einmal hinzu, um ihn zu strecken
-        stretched_matrix.append(stretched_row)
-
-    return stretched_matrix
-
-def draw_rects(canvas: ScalableCanvas, matrix: tuple[int, int, int]):
-     
-    # Draw outline
-    canvas.create_rectangle(1 * canvas.scale_factor-1, 1 * canvas.scale_factor * canvas.y_distortion-1,
-                                (len(matrix) + 1) * canvas.scale_factor + 1, 2 * (len(matrix[0]) + 1) * canvas.scale_factor * canvas.y_distortion + 1,
-                                fill="black", outline="grey")
-    
-    # Iterate through matrix
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-
-            # Extract color
-            rgb = matrix[i][j]
+    def _handle_sigint(self, signum, frame):
+        print("Ctrl+C detected. Stopping animations...")
+        self.keep_going = False
+        
+    ##### RUN METHOD #####
+    def run(self, time_per_anim):
+        
+        if self.remote_enabled:
+            self.read_auth()
+            if not self.username or not self.token:
+                exit(1)
+            self.ph = Pyghthouse(self.username, self.token)
+            self.ph.start()
             
-            # Add offset
-            x = i + 1
-            y = 2 * (j + 1)
-
-            # Draw colored rectangle
-            canvas.create_rectangle(x * canvas.scale_factor, y * canvas.scale_factor * canvas.y_distortion,
-                                    (x + 1) * canvas.scale_factor, (y + 1) * canvas.scale_factor * canvas.y_distortion,
-                                    fill="#%02x%02x%02x" % rgb)
-
-
-def update_canvas(canvas: ScalableCanvas, framequeue: multiprocessing.Queue, commandqueue: multiprocessing.Queue, root):
-
-    # Prepare timing parameters
-    update_interval = 1 / (canvas.fps_target) * 3
-    canvas.timer.set(update_interval)
-    wait = 1
-    
-    # Check if we have new frames
-    if not framequeue.empty():
-
-        # Get the newest frame that is ready
-        matrix = framequeue.get()
-        while not framequeue.empty(): 
-            matrix = framequeue.get_nowait()
-
-        # Draw matrix as rectangles on canvas
-        canvas.delete("all")
-        draw_rects(canvas, matrix)
+        animations = [
+                    GameOfLife(),
+                    Fireworks(), 
+                    Lavablobs(),
+                    #RgbTest(), 
+                    RainAnimation(), 
+                    ReboundAnimation(), 
+                    DiffAnimation(), 
+                    Bouncers(),
+                    ScrollText()]
         
-        # Keep animation process running
-        
-        if not timer.has_elapsed():
-            print(timer.remaining_ms())
-            commandqueue.put("keep_running")
-        else:
-            commandqueue.put("stop")  # Send stop command
-            root.destroy()            # Close the window and stop the program
-            return 
-
-        # Wait remaining time until the next frame is needed
-        wait = canvas.timer.remaining_ms(1)
-        
-
-    root.after(wait, update_canvas, canvas, framequeue, commandqueue, root)
+        anim_timer = Stopwatch()
+        frametimer = Stopwatch() 
+        update_interval = 1/(self.fps)
+        n = 0
+        if self.gui_enabled:
+            self.displayqueue = multiprocessing.Queue()
+            self.displayprocess = DisplayProcess(self.displayqueue,fps)
+            self.displayprocess.start()
+        while self.keep_going:
+            framequeue = multiprocessing.Queue()
+            commandqueue = multiprocessing.Queue()
+            anim_timer.set(time_per_anim)
+            anim = animations[n].get_instance(28, 27, framequeue, commandqueue, fps=fps)
+            print(f"Starting animation '{anim.name}' for {time_per_anim} seconds.")
+            #anim.params(28, 27, framequeue, commandqueue, fps=fps)
+            anim.start()
             
-
-            
-def main(animation=None):
-    
-    username, token = read_auth()
-    if not username or not token:
-        exit(1)
-        
-    animations = [Fireworks(), Lavablobs(), RgbTest(), RainAnimation(), ReboundAnimation(), DiffAnimation(), Bouncers()]
-    
-    text_input = ""
-    while not text_input or not text_input.split()[0].isnumeric():
-        
-        print("\nStart animation by typing:\n N --option")
-        
-        print("\nAvailable animations:")
-        print(" | ".join([f"[{i}] {anim.name}" for i, anim in enumerate(animations)]))
-        print("Available options: --gui")
-        
-        text_input = input()
-
-    options = text_input.split()[1:] if len(text_input.split()) > 1 else ""
-
-    fps = 40
-    
-    global timer
-    timer.set(10)
-    
-
-    framequeue = multiprocessing.Queue()
-    commandqueue = multiprocessing.Queue()
-
-
-    if '--gui' in options:
-        root = tk.Tk()
-        root.title("Lighthouse Animation")
-         
-        canvas = ScalableCanvas(root, width=28, height=28, bg="black")
-        canvas.pack(expand=tk.YES, fill=tk.BOTH)
-        canvas.fps_target = fps
-
-        animation_interval = 100  # Intervall in Millisekunden
-        root.after(animation_interval, update_canvas, canvas, framequeue, commandqueue, root)  # Erste Animation starten
-
-    n = int(text_input.split()[0])
-    anim = animations[n].get_instance(28, 27, framequeue, commandqueue, fps=fps, animspeed = 1)
-    anim.params(28, 27, framequeue, commandqueue, fps=fps, animspeed = 1)
-    anim.set_pyghthouse(username, token)
-    anim.start()
-    
-    if '--gui' in options:
-        root.mainloop()
-    else:    
-        while True:    
-            # Check if we have new frames
-            if not framequeue.empty():
-
+            image = Pyghthouse.empty_image()
+            opacity = 0
+            while not anim_timer.has_elapsed() and self.keep_going:   
+                frametimer.set(update_interval)
                 while not framequeue.empty(): 
-                    _ = framequeue.get_nowait()
-                
-                # Keep animation process running
+                    image = framequeue.get_nowait()
+                if opacity < 255:
+                    self.send_frame(image, opacity/256)
+                    opacity += 4
+                else:
+                    self.send_frame(image, 1.0)
                 commandqueue.put("keep_running")
-            time.sleep(0.1)
-            
-    anim.stop()
-    print("Thread joined.")
-    anim.close()
+                time.sleep(frametimer.remaining())
+                
+            for i in range(256, 0, -4):
+                frametimer.set(update_interval)
+                while not framequeue.empty(): 
+                    image = framequeue.get_nowait()
+                self.send_frame(image, i/256)
+                commandqueue.put("keep_running")
+                time.sleep(frametimer.remaining())
+                    
+            #Fireworks().terminate()
+            anim.stop()
+            anim.join(timeout = 2)
+            print("Animation terminated.")
+            n = (n+1) % len(animations)
+        if self.displayprocess:
+            print("Attempting to terminate subprocesses...")
+            self.displayprocess.stop()
+            self.displayprocess.join(timeout = 2)
+            if self.displayprocess.is_alive():
+                self.displayprocess.terminate()
+                self.displayprocess.join()  
+            else:
+                print("Display terminated successfully.")
+        exit(0)
+        
     
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        time_per_anim = int(sys.argv[1])
+        gui = False
+        remote = True
+        fps = 60
+        username = None
+        token = None
+        animation = None
+        for argument in sys.argv:
+            if '--local' in argument:
+                gui = True
+                remote = True
+            elif  '--gui' in argument:
+                gui = True
+            elif '--fps=' in argument and len(argument) > 6 and argument.split('=')[1].isnumeric():
+                fps = int(argument.split('=')[1])
+            elif '--env' in argument:
+                username = getenv('LIGHTHOUSE_USER')
+                token = getenv('LIGHTHOUSE_TOKEN')
+            elif '--animation=' in argument and len(argument) > 12 and argument.split('=')[1].isnumeric():
+                animation = int(argument.split('=')[1])
+        AnimationController(time_per_anim, gui, remote, username, token, fps, animation)
+    else:
+        print(f"Usage:\n{sys.argv[0]} [TIME] [OPTIONS]")
+        print("Whereas [TIME] = time in seconds and possible options are:")
+        print("--local\tRuns with local GUI only\n--gui\tRuns with both local GUI and remove connection")
+        print("--env\tRead 'username' and 'token' from environment variable (instead of file) as")
+        print("\tLIGHTHOUSE_USER | LIGHTHOUSE_TOKEN\n--fps=x\tRuns with x fps (default=60)")
