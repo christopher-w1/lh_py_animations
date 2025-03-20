f2d0f7c07fe8e63fdc5381950e0c21c0704dd2cd
Christopher Widling <christopher.widling@gmail.com>
2025-03-20 18:58:13 +0100

    integrated changes into main.py

--- src/main.py
+++ src/main.py
@@ -19,6 +19,17 @@ from animations.frauentag.venus_shiny import Venus_shiny
 from animations.frauentag.venus_variety import Venus_variety
 from animations.frauentag.venus_world import Venus_world
 from animations.frauentag.scrolltext_ft import Text_ft
+from animations.roots.axe import Axe
+from animations.roots.pot import Pot
+from animations.roots.roots_logo import Roots
+from animations.a_lighthouse import Lighthouse
+from animations.roots.hill import Hill
+from animations.roots.goat import Goat
+from animations.roots.text_kc import Text_kc
+from animations.roots.text_ks import Text_ks
+from animations.roots.text_roots import Text_roots
+from animations.roots.text_wtk import Text_wtk
+
 
 class AnimationController():   
     def __init__(self, time_per_anim, gui = False, remote = True, user=None, token=None, fps=60, animation=None) -> None:
@@ -145,16 +156,24 @@ class AnimationController():
             
         try:
             animations = [
-                Venus_world(),
-                Heart_ft(), # test with smooth edge
+                Text_kc(),
+                Text_ks(),
+                Text_roots(),
+                Text_wtk(),
+                Goat(),
+                Hill(),
+                Lighthouse(),
+                Roots(),
+                Pot(),
+                Axe(),
+                #Venus_world(),
+                #Heart_ft(), # test with smooth edge
                 #Venus_variety(), # maybe change some colors?
-                FireworksAnimation(),
-                Heart_ft(),
-                Text_ft(), # maybe alternative is better?
-                # put more animation inbetween?
-                Venus_shiny(),
-                
-
+                #FireworksAnimation(),
+                #Heart_ft(),
+                #Text_ft(), # maybe alternative is better?
+                ## put more animation inbetween?
+                #Venus_shiny(),
                 #Dots(),
                 #ScrollText(),
                 #ColorClashAnimation(),
