32ffeda9f5122f14f30a81ad14647509e4255932
Christopher Widling <christopher.widling@gmail.com>
2025-05-15 13:18:28 +0200

    change text wo 'world ibd day'

--- src/animations/ibd/text_ibd.py
+++ src/animations/ibd/text_ibd.py
@@ -21,7 +21,7 @@ class Text_ibd():
             img = Image.new("RGB", (28,14))
             draw = ImageDraw.Draw(img)
             draw.fontmode = "1"
-            draw.text((int(text_xOff),-2), "IBD Day", font_size=12, stroke_width=0)
+            draw.text((int(text_xOff),-2), "World IBD Day", font_size=12, stroke_width=0)
             for y in range(14):
                 for x in range(28):
                     if img.getpixel((x,y)) == (255,255,255):
@@ -35,7 +35,7 @@ class Text_ibd():
 
 
     def __init__(self,xsize=28, ysize=14, fps=30):
-        self.name = "Text - \"IBD Day\" - IBD"
+        self.name = "Text - \"World IBD Day\" - IBD"
         self.xsize = xsize
         self.ysize = ysize
         
