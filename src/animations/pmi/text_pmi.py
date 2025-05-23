d5078bf2da93c21e884b9bee61d13dffa2cc4c77
Nico Biernat <nico.biernat@gmail.com>
2025-05-23 16:37:17 +0200

    Cycle colors for PMI text and add firework to playlist

--- src/animations/pmi/text_pmi.py
+++ src/animations/pmi/text_pmi.py
@@ -9,83 +9,88 @@ class Text_PMI():
         next(self.text_PMI)
         return self.frame
 
-
     # Text: PMI
     def set_text_PMI(self):
         text_xOff = 3
+        color_idx = 0
+        frame_count = 0
         while True:
             img = Image.new("RGB", (28,14))
             draw = ImageDraw.Draw(img)
             draw.fontmode = "1"
             draw.text((int(text_xOff),-1), "PMI", font_size=12, stroke_width=0.2)
+            colors = [(128,128,128), (0,0,255), (255,0,0)]
             for y in range(14):
                 for x in range(28):
                     if self.bitmap[y][x] == "0":
                         self.frame[x][y] = (0,0,0)
                     if self.bitmap[y][x] == "1":
-                        self.frame[x][y] = (128,128,128)
+                        self.frame[x][y] = colors[color_idx]
                     if self.bitmap[y][x] == "2":
-                        self.frame[x][y] = (0,0,255)
+                        self.frame[x][y] = colors[(color_idx+1)%len(colors)]
                     if self.bitmap[y][x] == "3":
-                        self.frame[x][y] = (255,0,0)
-
+                        self.frame[x][y] = colors[(color_idx+2)%len(colors)]
+            frame_count += 1
+            if frame_count == self.fps:
+                frame_count = 0
+                color_idx = (color_idx + 1) % len(colors)
             # next(self.colors)
             yield None
 
     
-    def set_color(self):
-        change_color = self.change_color()
-        while True:
-            for y in range(14):
-                for x in range(28):
-                    if self.frame[x][y] == (255,255,255):
-                        color = next(change_color)
-                        self.frame[x][y] = color
-            yield None
+    # def set_color(self):
+    #     change_color = self.change_color()
+    #     while True:
+    #         for y in range(14):
+    #             for x in range(28):
+    #                 if self.frame[x][y] == (255,255,255):
+    #                     color = next(change_color)
+    #                     self.frame[x][y] = color
+    #         yield None
 
-    def change_color(self):
+    # def change_color(self):
         
-        while True:
-            # red to grey
-            color = [255,0,0]
-            while color[0] >= 128:
-                color[0] -= 0.1
-                color[1] += 0.1
-                color[2] += 0.1
-                yield color
-            # color = [127, 128, 128]
+    #     while True:
+    #         # red to grey
+    #         color = [255,0,0]
+    #         while color[0] >= 128:
+    #             color[0] -= 0.1
+    #             color[1] += 0.1
+    #             color[2] += 0.1
+    #             yield color
+    #         # color = [127, 128, 128]
             
-            # grey to blue
-            color = [128, 128, 127]
-            while color[0] >= 0:
-                color[0] -= 0.1
-                color[2] += 0.1
-                yield color
-            # color = [0, 128, 255]
+    #         # grey to blue
+    #         color = [128, 128, 127]
+    #         while color[0] >= 0:
+    #             color[0] -= 0.1
+    #             color[2] += 0.1
+    #             yield color
+    #         # color = [0, 128, 255]
 
-            # blue to grey
-            color = [0,128,255]
-            while color[2] >= 128:
-                color[0] += 0.1
-                color[2] -= 0.1
-                yield color
-            # color = [128, 128, 127]
+    #         # blue to grey
+    #         color = [0,128,255]
+    #         while color[2] >= 128:
+    #             color[0] += 0.1
+    #             color[2] -= 0.1
+    #             yield color
+    #         # color = [128, 128, 127]
 
-            # grey to red
-            color = [127, 128, 128]
-            while color[1] >= 0:
-                color[0] += 0.1
-                color[1] -= 0.1
-                color[2] -= 0.1
-                yield color
-            # color = [255, 0, 0]
+    #         # grey to red
+    #         color = [127, 128, 128]
+    #         while color[1] >= 0:
+    #             color[0] += 0.1
+    #             color[1] -= 0.1
+    #             color[2] -= 0.1
+    #             yield color
+    #         # color = [255, 0, 0]
 
 
     def __init__(self,xsize=28, ysize=14, fps=10):
         self.name = "Text - PMI"
         self.xsize = xsize
         self.ysize = ysize
-        
+        self.fps = fps
         self.frame = []
         for x in range(xsize):
             self.frame.append([])
@@ -93,7 +98,7 @@ class Text_PMI():
                 self.frame[x].append([0,0,0])
 
         self.text_PMI = self.set_text_PMI()
-        self.colors = self.set_color()
+        # self.colors = self.set_color()
         self.bitmap = ["0000000000000000000000000000",
                        "0000000000000000000000000000",
                        "1111111000002200000000220033",
